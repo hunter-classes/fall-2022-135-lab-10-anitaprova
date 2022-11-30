9bf22f4b1bad474eb3619e7d9d43056d6770b976
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-11-29 19:05:42 -0500

    added more tests

--- tests.cpp
+++ tests.cpp
@@ -37,23 +37,33 @@ TEST_CASE("TASK C") {
 TEST_CASE("TASK D") {
 	Movie movie1 = {"A Silent Voice", DRAMA, 130};
         Movie movie2 = {"Your Name", DRAMA, 106};
+	Movie movie3 = {"Mean Girls",COMEDY, 97};
 
 	TimeSlot t1 = {movie1, {5, 25}};
         TimeSlot t2 = {movie2, {1, 8}};
+	TimeSlot t3 = {movie3, {16, 56}};
 
 	CHECK(getTimeSlot(scheduleAfter(t1,movie2)) == "Your Name DRAMA (106 min) [starts at 7:35, ends by 9:21]");
 	CHECK(getTimeSlot(scheduleAfter(t2,movie1)) == "A Silent Voice DRAMA (130 min) [starts at 2:54, ends by 5:4]");
+	CHECK(getTimeSlot(scheduleAfter(t3,movie3)) == "Mean Girls COMEDY (97 min) [starts at 18:33, ends by 20:10]");
 }
 
 TEST_CASE("TASK E") {
 	Movie movie1 = {"A Silent Voice", DRAMA, 130};
         Movie movie2 = {"Your Name", DRAMA, 106};
 	Movie movie3 = {"Train to Busan", ACTION, 118};
+	Movie movie4 = {"Mean Girls",COMEDY, 97};
 
 	TimeSlot t1 = {movie1, {15, 4}};
         TimeSlot t2 = {movie2, {11, 19}};
 	TimeSlot t3 = {movie3, {15, 40}};
+	TimeSlot t4 = {movie4, {14, 44}};
 
-	CHECK(timeOverlap(t1, t2) == false);	
+	CHECK(timeOverlap(t1, t2) == false);
+	CHECK(timeOverlap(t4, t2) == false);
+	CHECK(timeOverlap(t2, t3) == false);	
 	CHECK(timeOverlap(t1, t3) == true);
+	CHECK(timeOverlap(t4, t1) == true);
+	CHECK(timeOverlap(t4, t3) == true);	
+
 }
